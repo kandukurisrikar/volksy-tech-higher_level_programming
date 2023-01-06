cb2d642978b696d145858173649e819d007285dc
root <ksrikar@brightcomgroup.com>
2023-01-06 02:56:41 -0800

    zero

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     lst = []
     for i in matrix:
-        s = map(lambda num:num **2, i)
+        s = map(lambda num: num**2, i)
         lst.append(list(s))
     print(lst)    
