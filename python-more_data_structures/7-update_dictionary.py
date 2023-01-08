8c652effb7d8efd8849356703a392fb4551757ae
root <ksrikar@brightcomgroup.com>
2023-01-08 02:02:59 -0800

    seventh

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    li = a_dictionary
+    for i in range(len(li)):
+        if li[i] == key:
+            li[i] = value
     return (a_dictionary)
