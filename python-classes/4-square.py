50a52c5e0215351b069d60f14b5722795c728d60
root <ksrikar@brightcomgroup.com>
2023-01-11 04:16:24 -0800

    fourth

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,6 +19,6 @@ class square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-        def area(self):
-            return self.__size * self.__size
+    def area(self):
+        return self.__size * self.__size
 
