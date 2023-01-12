a9ad95a44d44347ae2bb4767bc5eaa6cd6693e23
root <ksrikar@brightcomgroup.com>
2023-01-12 04:18:46 -0800

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """
-    Insert here module comment
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
 """
 
 
 def add_integer(a, b=98):
-    """ add two integers or floats """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
+    """
+    Checks if int, otherwise return sum
+    """
+    if type(a) == float or type(b) == float:
         a = int(a)
-    if type(b) is float:
         b = int(b)
-    return (a + b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
