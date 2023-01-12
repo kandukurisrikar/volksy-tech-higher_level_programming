f552bc8d242aa6b6382d1c8fe6c29d71d4fabd25
root <ksrikar@brightcomgroup.com>
2023-01-12 02:48:47 -0800

    hi

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Checks if int, otherwise return sum
+    """
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
