0833d8dbe5f3e39a6dadedae291abfc9c01d1ce9
root <ksrikar@brightcomgroup.com>
2023-01-18 02:34:16 -0800

    hi

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""json"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """json save"""
+
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return(f.write(json.dumps(my_obj)))
