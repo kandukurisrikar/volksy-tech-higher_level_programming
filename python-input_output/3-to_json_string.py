56ce2392b15350dff0e557d16b61d10e1c67bf17
root <ksrikar@brightcomgroup.com>
2023-01-18 01:44:18 -0800

    hi

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""json"""
-
-
+"""to_json_string
+"""
 import json
 
 def to_json_string(my_obj):
-    """return format"""
+    """Return JSON format of my_obj
+    """
 
-
-    return to_json_string(my_obj):
+    return json.dumps(my_obj)
