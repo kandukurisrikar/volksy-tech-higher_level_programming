97de778e4166a1d35fb8b072824fcbbe5aeec386
root <ksrikar@brightcomgroup.com>
2023-01-31 08:25:24 -0800

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- HI
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
-	);
+USE hbtn_0d_usa;
+-- Creates table `cities` with fields `id`, `state_id`, and `name`.
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id)
