22fea55febd72a12240ebf7eba12959fd0664e16
root <ksrikar@brightcomgroup.com>
2023-01-31 09:15:50 -0800

    10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- HI
-SELECT tv_shows.title,tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres.genre_id = tv_shows.id
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
 ~
 ~
