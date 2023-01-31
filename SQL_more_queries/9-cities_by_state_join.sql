a9fda3430a4447376854190b2200b3955cfa1aa6
root <ksrikar@brightcomgroup.com>
2023-01-31 09:01:01 -0800

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- HI
-SELECT cities.id,cities.name,states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 LEFT JOIN states
-ON states.id = city.states_id
+ON states.id = cities.state_id
 ORDER BY cities.id;
