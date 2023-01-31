d2c2dde545fb29a723f65fe21fcb28c2ae0b148e
root <ksrikar@brightcomgroup.com>
2023-01-31 09:13:17 -0800

    10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,60 @@
+-- HI
+SELECT tv_shows.title,tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres.genre_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                                                                                                                    4,37          All
+ 
