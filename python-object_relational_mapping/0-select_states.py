6472a6d2045051636298ad66eb22917ce4803316
root <ksrikar@brightcomgroup.com>
2023-02-06 21:37:37 -0800

    hi

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
-#!/usr/bin/pythton3
+#!/usr/bin/python3
 '''connections'''
 
 
 import sys
 import MySQLdb
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], bd=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM 'states'")
