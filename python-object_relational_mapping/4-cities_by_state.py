3bd8e405c5c71e09428afa7849a141fb9d8aa015
root <ksrikar@brightcomgroup.com>
2023-02-11 09:49:40 -0800

    4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,16 +2,22 @@
 
 
 
-import sys
-import MySQLdb
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
+    if len(sys.argv) != 4:
+        print('Use: 0-select_states.py <mysql username> <mysql password>'
+              ' <database name>')
+        sys.exit()
 
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities '
+                'LEFT JOIN states ON cities.state_id = states.id;')
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
