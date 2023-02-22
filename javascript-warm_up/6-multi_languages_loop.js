7fdd3d62ae45be9e25cdba267edcbff80de9ded3
kandukurisrikar <ksrikar@brightcomgroup.com>
2023-02-21 20:52:43 -0800

    6js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
-
-const lang = 'C is fun';
-
-if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(lang);
-  }
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
